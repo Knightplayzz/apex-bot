b1653ce28e9a846083aa0f83bdac7efb46b30667
Knightplayzz <73634415+Knightplayzz@users.noreply.github.com>
2024-06-04 07:46:52 +0200

    update images

--- src/events/interactions/slashCommands.js
+++ src/events/interactions/slashCommands.js
@@ -47,8 +47,8 @@ module.exports = {
                 var isServerPremium, isUserPremium;
                 const entitlements = await interaction.client.application.entitlements.fetch(interaction.guild);
                 const userEntitlements = await interaction.client.application.entitlements.fetch(interaction.user);
-                userEntitlements.forEach(async data => { if (data.skuId === '1246777340806303866') isUserPremium = true }); //|| data.skuId === '1014207340188270673' THIS IS USER SUBSCRIPTION
-                entitlements.forEach(async data => { if (data.skuId === '1247194389281902683' || data.skuId === '1246772758541631518') isServerPremium = true; }) //LIFETIME 1247194389281902683| sub 1246772758541631518
+                userEntitlements.forEach(async data => { if (data.skuId === '1246777340806303866' && data.userId === interaction.user.id) isUserPremium = true }); //|| data.skuId === '1014207340188270673' THIS IS USER SUBSCRIPTION
+                entitlements.forEach(async data => { if (data.skuId === '1247194389281902683' && data.guildId === interaction.guild.id || data.skuId === '1246772758541631518' && data.guildId === interaction.guild.id) isServerPremium = true; }) //LIFETIME 1247194389281902683| sub 1246772758541631518
                 if (isServerPremium == undefined && isUserPremium == undefined) {
                     let hasUserVotedVar = await hasUserVoted(interaction, langOpt);
                     if (hasUserVotedVar == true) return await slashCommand.execute(interaction, auth, langOpt)
